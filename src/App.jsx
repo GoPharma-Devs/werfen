edafcb7eeeccbc21f45afa1b62138e713bac8252
Carlos Boyzo <carlos.boor@gmail.com>
2022-05-12 16:56:55 -0500

    ok

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+      <div className='transmision' style={{padding: '56.25% 0 0 0', position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center'}}><iframe src="https://vimeo.com/event/2111881/embed" frameBorder={0} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', padding: '2%', boxSizing: 'border-box', margin: 'auto'}} title="WerfenTalks" /></div>
+
+      </header>
+    </div>
+  )
+}
+
+export default App
