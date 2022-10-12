bdafcac8cf4407e72bc6d48843ec196b7788f6cc
Carlos Boyzo <50590301+crbzxy@users.noreply.github.com>
2022-10-12 00:21:36 -0500

    delete unused state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react'
+import React from 'react'
 import logo from './logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
