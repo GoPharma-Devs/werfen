127fd066bc41488a1dd6afaa19fd988a9255d8ef
Carlos Boyzo <50590301+crbzxy@users.noreply.github.com>
2022-10-13 15:50:25 -0500

    update transmision

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-      <div className='transmision' style={{padding: '56.25% 0 0 0', position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center'}}><iframe src="https://vimeo.com/event/2507467/embed" frameBorder={0} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{position: 'absolute', top: 0, left: 0, padding: '5%', boxSizing: 'border-box', margin: 'auto'}} title="WerfenTalks" /></div>
+      <div className='transmision' style={{padding: '56.25% 0 0 0', position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center'}}><iframe src="https://vimeo.com/event/2511548/embed" frameBorder={0} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{position: 'absolute', top: 0, left: 0, padding: '5%', boxSizing: 'border-box', margin: 'auto'}} title="WerfenTalks" /></div>
 
       </header>
     </div>
